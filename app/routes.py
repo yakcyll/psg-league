4488ab8a95167497ca168d8af1e76241ad3bceda
yakcyll~ <yakcyll@gmail.com>
2019-09-08 20:11:56 +0200

    Extended validation and url_for.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from forms.login_form import LoginForm
 
@@ -28,5 +28,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Logowanie', form=form)
