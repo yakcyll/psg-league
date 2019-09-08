4efbb71d334a8055272cab9b3ac9f5dfc434c578
yakcyll~ <yakcyll@gmail.com>
2019-09-08 18:59:10 +0200

    Templates.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'world'}
+    return render_template('index.html', title='Landing', user=user)
+
+@app.route('/news')
+def news():
+    posts = [
+        {
+            'author': {'username': 'who'},
+            'body': 'what',
+        },
+        {
+            'author': {'username': 'where'},
+            'body': 'when',
+        },
+    ]
+
+    return render_template('news.html', title='News', posts=posts)
