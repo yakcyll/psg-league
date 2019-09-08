8dcf94c61917c4f785d0212607b2fd2289771550
yakcyll~ <yakcyll@gmail.com>
2019-09-08 20:51:06 +0200

    Database models and interactive console.

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
+import os
+
 from sensitive_config import SensitiveConfig
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(SensitiveConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
