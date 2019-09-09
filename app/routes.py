fa282d0d1e426a35d822badbdcbee17cee79e1f3
yakcyll~ <yakcyll@gmail.com>
2019-09-09 20:51:11 +0200

    Logging in.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
 from app import app
+from app.models import User
 from forms.login_form import LoginForm
 
 @app.route('/')
@@ -25,8 +27,22 @@ def news():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Niewłaściwa nazwa użytkownika bądź hasło')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
+
     return render_template('login.html', title='Logowanie', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
