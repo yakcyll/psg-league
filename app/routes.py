addf9a0cc5f1454371696c7123db49dc474e6434
yakcyll~ <yakcyll@gmail.com>
2019-10-23 23:49:11 +0200

    Last seen on-line.

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
+from datetime import datetime
+
 from app import app, db
 from app.models import User
 from forms.login_form import LoginForm
@@ -81,3 +83,9 @@ def user(username):
         {'author': user, 'body': 'Test post #2'},
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
